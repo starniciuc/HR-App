0d316af6e18f69a2757b5ddd002388769fb11ec1
elboraki <burak7younes@gmail.com>
2015-01-14 21:13:23 +0000

    add a home page

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="css/custom.css"/>
+
+  </head>
+  <body>
+    <div class="wrapper">
+
+    </div>
+    <script language="javascript" src="js/bootstrap.min.js"></script>
+    <script language="javascript" src="js/jquery.js"></script>
+
+  </body>
+</html>
