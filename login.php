281518b52092e56b9a7b6a5bd7d1b4202de3ad73
elboraki <burak7younes@gmail.com>
2015-01-15 11:39:53 +0000

    for verification login

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+require_once("conn.php");
+$login=$_POST['username'];
+$pass=$_POST['userpass'];
+$sql="select login,pass from users where login=:login and pass=:pass";
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$stmt=$conn->prepare($sql);
+$stmt->bindParam(':login',$login,PDO::PARAM_STR);
+$stmt->bindParam(':pass',$pass,PDO::PARAM_STR);
+$stmt->execute();
+$count=$stmt->rowCount();
+if($count==0){
+echo("Your username or password is not correct ");
+echo('<a href="index.php">Go back</a>');
+
+}
+else{
+  header("location:dashbord.php");
+}
+
+
+
+?>
