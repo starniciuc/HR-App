5b302ba29167a2e9915f540ce4e4641e6d9da1f3
elboraki <burak7younes@gmail.com>
2015-01-20 19:57:26 +0000

    new file for dashbord

---
+++ dashbord.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+  <link rel="stylesheet" type="text/css" href="css/custom.css"/>
+  <style type="text/css">
+  ul li{
+    top:14px;
+    border-bottom:1px solid gray;
+  }
+
+  </style>
+
+</head>
+<body>
+  <div id="header">
+    <div class="wrapper">
+      <h1>HR-Application</h1>
+    </div>
+  </div>
+  <div id="content">
+    <div class="wrapper">
+      <ul class="nav nav-pills">
+        <li role="presentation" class="active"><a href="dashbord.php">Home</a></li>
+        <li role="presentation"><a href="employee.php">Employees</a></li>
+        <li role="presentation"><a href="interns.php">Interns</a></li>
+        <li role="presentation"><a href="dept.php">Departement</a></li>
+      </ul>
+
+    </div>
+  </div>
+  <div id="footer">
+    <div class="wrapper">
+
+    </div>
+  </div>
+
+
+  <script language="javascript" src="js/bootstrap.min.js"></script>
+  <script language="javascript" src="js/jquery.js"></script>
+
+</body>
+</html>
