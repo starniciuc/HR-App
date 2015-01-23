f5a4927d9d436deb16f975ce77f88305da0153e1
elboraki <burak7younes@gmail.com>
2015-01-23 21:37:25 +0000

    script for delete records of employees table

---
+++ delemp.php
@@ -0,0 +1,10 @@
+<?php
+require_once("conn.php");
+$id=$_GET["id"];
+$sql="delete from emp where empno='".$id."'";
+$stmt=$conn->prepare($sql);
+$stmt->execute();
+header("location:employees.php");
+
+
+?>
