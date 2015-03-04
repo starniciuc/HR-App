c07192f01a38fd438ac53dbd8d83714d5cd5791d
elboraki <burak7younes@gmail.com>
2015-03-04 23:23:15 +0000

    add script for display datepicker in 2 textbox

--- js/custom.js
+++ js/custom.js
@@ -14,4 +14,6 @@
 
 
   });
-  $('#hiredate').datepicker({dateFormat:"yy-mm-dd"});
+  $('#hiredate').datepicker({dateFormat:"yy-mm-dd",navigationAsDateFormat: true,showButtonPanel: true});
+  $('#dateto').datepicker({dateFormat:"yy-mm-dd",navigationAsDateFormat: true,showButtonPanel: true});
+  $('#datefrom').datepicker({dateFormat:"yy-mm-dd",navigationAsDateFormat: true,showButtonPanel: true});
